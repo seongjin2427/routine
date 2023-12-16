0a743768db9eccf9eafde977fd6782e816916460
seongjin <seongjin2427@gmail.com>
2023-12-17 08:14:29 +0900

    2023.12.17. baekjoon 1541. 잃어버린 괄호 unsolved

--- cote/baekjoon/1541/1541.test.ts
+++ cote/baekjoon/1541/1541.test.ts
@@ -1,16 +1,24 @@
 import solution from "./1541";
 
 describe("1541. 잃어버린 괄호", () => {
-  it("case 1. 일반 케이스 1: 부호가 다름", () => {
+  it("case 1. 뺄셈이 먼저 나오는 경우", () => {
     const input = "55-50+40";
-    expect(solution(input)).toBe(-35);
+
+    expect(solution(input)).toEqual(-35);
   });
-  it("case 2. 일반 케이스 2: 부호가 모두 동일함", () => {
+  it("case 2. 덧셈만 있는 경우", () => {
     const input = "10+20+30+40";
-    expect(solution(input)).toBe(100);
+
+    expect(solution(input)).toEqual(100);
   });
-  it("case 3. 0으로 시작하는 숫자 포함", () => {
+  it("case 3. 0부터 시작하지만, 연산되는 값은 0이 아닌 경우", () => {
     const input = "00009-00009";
-    expect(solution(input)).toBe(0);
+
+    expect(solution(input)).toEqual(0);
+  });
+  it("case 4. 뺄셈이 나중에 나오는 경우", () => {
+    const input = "55+50-40";
+
+    expect(solution(input)).toEqual(65);
   });
 });
